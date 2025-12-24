1ca9fc81e0171a44e19ce28989fa46c2bf1d729c
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-24 15:59:05 -0300

    feat: changing the returned JSON for a manual test

--- src/controllers/podcast-controller.ts
+++ src/controllers/podcast-controller.ts
@@ -4,9 +4,24 @@ export const getrListEpisodes  = async (req: IncomingMessage, res: ServerRespons
     res.writeHead(200, {'Content-type': 'application/json'});
     res.end(
         JSON.stringify(
-            {
-                name: "André"
-            }
+            [
+                {
+                  "podcastName": "flow",
+                  "episode": "CBUM - Flow #319",
+                  "videoId": "pQSuQmUfS30",
+                  "cover": "https://i.ytimg.com/vi/pQSuQmUfS30/maxresdefault.jpg",
+                  "link": "https://www.youtube.com/watch?v=pQSuQmUfS30",
+                  "categories": ["saúde", "esporte", "bodybuilder"]
+                },
+                {
+                  "podcastName": "flow",
+                  "episode": "RUBENS BARRICHELLO - Flow #339",
+                  "videoId": "4KDGTdiOV4I",
+                  "cover": "https://i.ytimg.com/vi/4KDGTdiOV4I/maxresdefault.jpg",
+                  "link": "https://www.youtube.com/watch?v=4KDGTdiOV4I",
+                  "categories": ["esporte", "corrida"]
+                }
+            ]
         )
     );
 }
