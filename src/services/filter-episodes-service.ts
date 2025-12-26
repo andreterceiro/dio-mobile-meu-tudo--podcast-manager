a03b053a09c980b389f78b79fdc5a6b3105b072b
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-26 10:42:55 -0300

    feat: now we extract the podcsastName from the URL when filtering in the service

--- src/services/filter-episodes-service.ts
+++ src/services/filter-episodes-service.ts
@@ -1,6 +1,8 @@
 import { repositoryPosdcast } from "../repositories/podcasts-repository";
 
-export const serviceFilterEpisodes = async (podcastName: string) => {
+export const serviceFilterEpisodes = async (url: string | undefined) => {
+    let podcastName = url?.split("?p=")[1] ?? "";
+
     const data = await repositoryPosdcast(podcastName);
     return data;
 }
