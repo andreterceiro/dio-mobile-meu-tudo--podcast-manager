152d0caf35b5e129bc353a7df9179214140785b0
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-26 07:53:48 -0300

    feat: in the repository, allowing to filter a podcast by its name

--- src/repositories/podcasts-repository.ts
+++ src/repositories/podcasts-repository.ts
@@ -4,8 +4,16 @@ import Podcast from "../models/Podcast";
 
 const pathData = _path.join(__dirname, "../repositories/podcasts.json");
 
-export const repositoryPosdcast = async (): Promise<Podcast[]> => {
+export const repositoryPosdcast = async (podcastName?: string): Promise<Podcast[]> => {
     const rawdata = fs.readFileSync(pathData);
-    const jsonFile = JSON.parse(rawdata.toString());
+    let jsonFile = JSON.parse(rawdata.toString());
+
+    if (podcastName) {
+        jsonFile = jsonFile.filter(
+            (podcastEntry: Podcast) => {
+                podcastEntry.podcastName === podcastName;
+            }
+        );
+    }
     return jsonFile;
 }
