133c8ca7d3a46482336398bc3529b9b5e7079c6c
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-26 11:36:02 -0300

    feat: now server.ts only starts the HTTP server and call a file the deal with the routes, calling the right controller methods

--- src/server.ts
+++ src/server.ts
@@ -2,24 +2,11 @@ import * as http from "http";
 import {getListEpisodes, getFilterEpisodes} from "./controllers/podcast-controller";
 import { Routes } from "./routes/routes";
 import { HttpMethods } from "./utils/http-methods";
+import { app } from "./app";
 
 const port = process.env.PORT;
 
-const server = http.createServer(
-    async (
-        request, 
-        response
-    ) => {
-        const [baseUrl, queryString] = request.url?.split("?") ?? ["", ""];
-        if (request.method === HttpMethods.GET && baseUrl === Routes.LIST) {
-            await getListEpisodes(request, response);
-        }
-
-        if (request.method === HttpMethods.GET && baseUrl === Routes.EPISODE) {
-            await getFilterEpisodes(request, response);
-        }
-    }
-);
+const server = http.createServer(app);
 
 server.listen(
     port,
