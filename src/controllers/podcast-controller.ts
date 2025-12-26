5200f40cc851c350978bc3213a0b8e5d691da700
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-26 12:31:42 -0300

    feat: adjusting the controlller to the new format returned by the service

--- src/controllers/podcast-controller.ts
+++ src/controllers/podcast-controller.ts
@@ -3,6 +3,7 @@ import {serviceListEpisodes} from "../services/list-episodes-service";
 import {serviceFilterEpisodes} from "../services/filter-episodes-service";
 import { HttpStatusCodes } from "../utils/http-status-codes";
 import { ContentType } from "../utils/content-type";
+import { FilterPodcastModel } from "../models/filter-podcast-model";
 
 export const getListEpisodes  = async (req: IncomingMessage, res: ServerResponse) => {
     res.writeHead(HttpStatusCodes.OK, {'Content-type': ContentType.JSON});
@@ -14,11 +15,11 @@ export const getListEpisodes  = async (req: IncomingMessage, res: ServerResponse
 }
 
 export const getFilterEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
-    const content = await serviceFilterEpisodes(req.url);
-    res.writeHead(HttpStatusCodes.OK, {'Content-type': ContentType.JSON});
+    const response: FilterPodcastModel = await serviceFilterEpisodes(req.url);
+    res.writeHead(response.statusCode, {'Content-type': ContentType.JSON});
     res.end(
         JSON.stringify(
-            content
+            response.body
         )
     )
 }
