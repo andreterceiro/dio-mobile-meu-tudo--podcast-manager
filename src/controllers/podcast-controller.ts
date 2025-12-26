c446f3c00325d998556dd6e104f2b861b3ff3eb3
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-26 11:34:50 -0300

    feat: using the file with the valid content types

--- src/controllers/podcast-controller.ts
+++ src/controllers/podcast-controller.ts
@@ -2,9 +2,10 @@ import {IncomingMessage, ServerResponse} from "http";
 import {serviceListEpisodes} from "../services/list-episodes-service";
 import {serviceFilterEpisodes} from "../services/filter-episodes-service";
 import { HttpStatusCodes } from "../utils/http-status-codes";
+import { ContentType } from "../utils/content-type";
 
 export const getListEpisodes  = async (req: IncomingMessage, res: ServerResponse) => {
-    res.writeHead(HttpStatusCodes.OK, {'Content-type': 'application/json'});
+    res.writeHead(HttpStatusCodes.OK, {'Content-type': ContentType.JSON});
     res.end(
         JSON.stringify(
             await serviceListEpisodes()
@@ -14,7 +15,7 @@ export const getListEpisodes  = async (req: IncomingMessage, res: ServerResponse
 
 export const getFilterEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
     const content = await serviceFilterEpisodes(req.url);
-    res.writeHead(HttpStatusCodes.OK, {'Content-type': 'application/json'});
+    res.writeHead(HttpStatusCodes.OK, {'Content-type': ContentType.JSON});
     res.end(
         JSON.stringify(
             content
