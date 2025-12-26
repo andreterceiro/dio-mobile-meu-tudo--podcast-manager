fbe23b34aaf7afe9ddc245e83f19c4a59ee58606
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-26 12:30:49 -0300

    feat: now the service determines the HTTP status code on filtering a podcast by name

--- src/services/filter-episodes-service.ts
+++ src/services/filter-episodes-service.ts
@@ -1,8 +1,23 @@
+import { FilterPodcastModel } from "../models/filter-podcast-model";
 import { repositoryPodcast } from "../repositories/podcasts-repository";
+import { HttpStatusCodes } from "../utils/http-status-codes";
 
-export const serviceFilterEpisodes = async (url: string | undefined) => {
+export const serviceFilterEpisodes = async (url: string | undefined): Promise<FilterPodcastModel> => {
     let podcastName = url?.split("?p=")[1] ?? "";
+    let responseFormat: FilterPodcastModel = {
+        statusCode: 0,
+        body: []
+    }
 
     const data = await repositoryPodcast(podcastName);
-    return data;
+
+    responseFormat.body = data;
+    if (data.length !== 0
+        ) {
+        responseFormat.statusCode = HttpStatusCodes.OK;
+    } else {
+        responseFormat.statusCode = HttpStatusCodes.NO_CONTENT;
+    }
+
+    return responseFormat;
 }
